77ea2174ecb9f59c95edfb51f95357ae190e628d
yusezmann <caritaku.inc@gmail.com>
2021-07-06 16:35:16 +0700

    first commit

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+      <v-toolbar app color="transparent" dark>
+          <v-toolbar-title>
+              <v-img src="https://th.bing.com/th/id/R.829c5d7d60ce103dceb63c7914b9b266?rik=0DNFHSvAH9CAmA&riu=http%3a%2f%2fpngimg.com%2fuploads%2ftesla_logo%2ftesla_logo_PNG21.png&ehk=tHaAfgBpaGpSMisvvI5D6%2fdfm5S1W%2fFQSZZFSJ4Cvzw%3d&risl=&pid=ImgRaw" height="110px" width="200px"></v-img>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn class="hidden-sm-and-down black font-weight-bold white--text">Specifications</v-btn>
+          <v-btn class="hidden-sm-and-down black font-weight-bold white--text ml-3">Order Now</v-btn>
+
+      </v-toolbar>
+  </span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Promo />
+    <Specifications />
+    <Interior />
+    <CoreFeatures />
+    <OrderNow />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Promo from '../components/Promo.vue'
+import Specifications from '../components/Specifications.vue'
+import Interior from '../components/Interior.vue'
+import CoreFeatures from '../components/CoreFeatures.vue'
+import OrderNow from '../components/OrderNow.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Promo,
+    Specifications,
+    Interior,
+    CoreFeatures,
+    OrderNow
   }
 }
 </script>
